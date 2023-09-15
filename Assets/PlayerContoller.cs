047a6dc74b1f101a1cb31a8e28c4fcfddd4a90ce
sssmart24 <ssmart24@aoiths.org>
2023-09-15 12:35:40 -0400

    1.2

---
+++ Assets/PlayerContoller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+    float speed = 20; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the vehicle foward 
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
