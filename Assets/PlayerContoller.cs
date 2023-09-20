99166d9894fe5d432277435ac6e10532f0d57b5a
sssmart24 <ssmart24@aoiths.org>
2023-09-20 12:33:59 -0400

    1.4

--- Assets/PlayerContoller.cs
+++ Assets/PlayerContoller.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    public float speed = 5.0f; 
-    
+    private float speed = 20.0f; 
+    private float turnSpeed = 45.0f;  
+    private float horizontalInput; 
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,11 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //move the vehicle foward 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move the vehicle forward based on vertical input 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
