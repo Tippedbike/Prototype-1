e0fd0d81c2286aaaa6d57610cab5bebe568b4e26
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-03-20 13:21:04 -0400

    idk

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera Camera2;
+
+    public void ShowCamera2View()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Camera2.enabled = true; 
+        }
+    }
+}
