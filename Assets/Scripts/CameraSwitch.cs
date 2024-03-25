ad3949cebe981eda9ab813c2248d87239ad0924f
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-03-25 12:36:23 -0400

    done

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class CameraSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera Camera1;
     public Camera Camera2;
 
-    public void ShowCamera2View()
+    void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Camera2.enabled = true; 
-        }
+        
     }
-}
+    void Update()
+    {
+        FirstPersonCamera();
+    }
+     public void FirstPersonCamera()
+    {
+         
+         
+        if (Input.GetKeyDown(KeyCode.P))
+       {
+            if ( Camera2.enabled == true )
+            {
+                Camera1.enabled = true;
+                Camera2.enabled = false; 
+                return;
+            }else if (Camera1.enabled == true)
+                {
+                    Camera2.enabled = true;
+                    Camera1.enabled = false;
+                    return;
+                } 
+       }
+    } 
+
+} 
--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,12 +7,10 @@ public class FollowPlayer : MonoBehaviour
     // Start is called before the first frame update 
     public GameObject player; 
     private Vector3 offset = new Vector3(0, 5, -7);
-
-    
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-    }
+    } 
     
 }
